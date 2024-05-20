e7b4dadbb747a188f5a1d006efde329c461efedb
bayupratama15 <bolehjugaemail@gmail.com>
2024-05-20 16:40:34 +0700

    chore: Update VotingAddress in constants.js

--- context/constants.js
+++ context/constants.js
@@ -2,5 +2,6 @@
 
 import voting from "./Create.json";
 
-export const VotingAddress = "0x44179eFc89A216B7E938Ccbe22E7878dAa489709";
+// export const VotingAddress = "0x44179eFc89A216B7E938Ccbe22E7878dAa489709";
+export const VotingAddress = "0x226c022dD3f55793D630B94318b32a9319F6C973";
 export const VotingAddressABI = voting.abi;
